97f5046c3ec20cee749d6a063e09483980c77f49
Vihan Perera <vihanpereraux@gmail.com>
2023-06-16 14:05:20 +0530

    initial commit

---
+++ hands-on-script/assets/js/script.js
@@ -0,0 +1,11 @@
+// create a new hydra-synth instance
+var hydra = new Hydra({ detectAudio: false })
+osc(5, 0.9, 0.001)
+    .kaleid([3,4,5,7,8,9,10].fast(0.1))
+    .color(0.5, 0.3)
+    .colorama(0.4)
+    .rotate(0.009,()=>Math.sin(time)* -0.001 )
+    .modulateRotate(o0,()=>Math.sin(time) * 0.003)
+    .modulate(o0, 0.9)
+    .scale(0.9)
+    .out(o0)
