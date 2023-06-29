fc44aae4c664b2d31009514286e3b91ceea17ff8
Vihan Perera <vihanpereraux@gmail.com>
2023-06-29 17:04:20 +0530

    Change folder structure

--- hydra-vite-installation/hydra-vite-installation/main.js
+++
@@ -1,8 +0,0 @@
-var hydra = new Hydra({ detectAudio: false })
-
-osc(40,0.2,1)
-  .modulateScale(osc(40,0,1).kaleid(8))
-  .repeat(2,4)
-  .modulate(o0,0.05)
-  .modulateKaleid(shape(4,0.1,1))
-  .out(o0)
